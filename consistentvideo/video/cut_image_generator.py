1e189388f93e1e8abc1fe112a5d9a7043c441495
rachihyeon <skallook@hanmail.net>
2025-08-05 03:13:09 +0900

    refactor: 컷 이미지 생성부 리팩토링
    - cut_image_generator.py내 이미지 생성 알고리즘 삭제(프롬프트 구성 알고리즘만 존재)
    - 사용되지 않는 주석 삭제
    - CutImageGenerator내 __create_client메서드 삭제
    - Entity정보 추출 알고리즘에서 image descriptor생성 로직 삭제 및 image path 생성 로직 추가
    - model_selector.py의 CutImageGeneratorModelSelector의 call_CutImageGenerator_ai로부터 원하는 모델 생성 기능 추가

--- consistentvideo/video/cut_image_generator.py
+++ consistentvideo/video/cut_image_generator.py
@@ -1,19 +1,14 @@
-from openai import OpenAI
-import base64
-import json
-from PIL import Image
 from io import BytesIO
 from .base import CutImageGeneratorBase
-import requests
+from .model_selector import *
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-
 class CutImageGenerator(CutImageGeneratorBase):
     def __init__(self, scene_num: int, cut: dict, output_path: str, entity_image_path: str,
-                 entity: list = None):  # cut = dict, entity = list<tuple(type, name, desc, image_path)>
+                 entity: list = None, ai_model: str = 'gpt'):  # cut = dict, entity = list<tuple(type, name, desc, image_path)>
         '''
         ex)
         cut = {'cut_id': 1, 'description': '버스 정류장에서 버스가 도착하는 장면.', 'characters': [], 'background': '조용한 아침 거리와 버스 정류장.', 'objects': ['버스', '버스 정류장']}
@@ -24,22 +19,11 @@ class CutImageGenerator(CutImageGeneratorBase):
         ]
         '''
         super().__init__(entity)
-        self.ai_model = self.__create_client()
         self.scene_num = scene_num
         self.cut = cut
         self.entity_image_path = entity_image_path
         self.output_path = output_path
-
-    def __create_client(self) -> OpenAI:
-        api_key = os.getenv("OPENAI_API_KEY")
-        return OpenAI(api_key=api_key)
-
-    # def __load_gpt_api_key(self) -> str:
-    #     try:
-    #         with open("api_key.txt", "r") as file:
-    #             return file.read().strip()
-    #     except FileNotFoundError:
-    #         raise RuntimeError("api key doesn't exist")
+        self.ai_model = ai_model
 
     def execute(self):
         if not self.cut:
@@ -55,27 +39,20 @@ class CutImageGenerator(CutImageGeneratorBase):
 
         # Get entities that will be used
         prompt_entity_parts = []
-        image_files = []
+        image_paths = []
 
         if self.entity:
             for e_type, name, attrs, image_filename in self.entity:
-            #     print(f"e_type={e_type}, name={name}, attrs={attrs}, image_filename={image_filename}")
-            #     print(f"self.cut.get('character', [])={self.cut.get('character', [])}")
-            #     print(f"self.cut.get('object', [])={self.cut.get('object', [])}")
-            #     print(f"self.cut.get('location', '')={self.cut.get('location', '')}")
                 if (
-                        (e_type == "character" and name in self.cut.get("character", [])) or
-                        (e_type == "object" and name in self.cut.get("object", [])) or
-                        (e_type == "location" and name == self.cut.get("location", ""))
+                    (e_type == "character" and name in self.cut.get("character", [])) or
+                    (e_type == "object" and name in self.cut.get("object", [])) or
+                    (e_type == "location" and name == self.cut.get("location", ""))
                 ):
                     desc = f"['{e_type}': '{name}', 'attribute': {attrs}]"
                     prompt_entity_parts.append(desc)
                     print(f"/////desc: {desc}")
                     image_path = os.path.join(self.entity_image_path, image_filename)
-                    if not os.path.exists(image_path):
-                        raise FileNotFoundError(f"Entity image file not found: {image_path}")
-                    image_files.append(open(image_path, "rb"))
-                    print(f"/////image_path: {image_path}")
+                    image_paths.append(image_path)
 
         # Input prompt composing
         entity_prompt = " ".join(prompt_entity_parts)
@@ -86,72 +63,20 @@ class CutImageGenerator(CutImageGeneratorBase):
             "among the character, object, and background attributes."
         )
 
-        # ---------------dalle3 (can also dalle2)--------------
-
-        # result = self.ai_model.images.generate(
-        #     model="dall-e-3",
-        #     prompt=prompt,
-        #     size="1792x1024"
-        # )
-        # image_url = result.data[0].url
-        # image_bytes = requests.get(image_url).content
-        # self.cut_image = Image.open(BytesIO(image_bytes))
-
-        # -----------------------------------------------------
-        # 
-        # ---------------------gpt-image-1---------------------
         print(f"S{self.scene_num:04d}-C{cut_id:04d}.png")
-        print(f"image_files: {image_files}")
-        if image_files:
-            result = self.ai_model.images.edit(
-                model="gpt-image-1",
-                image=image_files,
-                prompt=prompt,
-                quality="low",  # high/medium/low
-                size="1536x1024"  # There is no size compatible 16:9, need to adjust by prompt
-            )
-        else:
-            result = self.ai_model.images.generate(
-                model="gpt-image-1",
-                prompt=prompt,
-                quality="low",  # high/medium/low
-                size="1536x1024"  # There is no size compatible 16:9, need to adjust by prompt
-            )
-            # -----------------------------------------------------
-
-        # closing file descriptor 
-        for f in image_files:
-            f.close()
-
-        image_base64 = result.data[0].b64_json
-        image_bytes = base64.b64decode(image_base64)
-        self.cut_image = Image.open(BytesIO(image_bytes))
+        print(f"image_paths: {image_paths}")
+        
+        image_generator_model = CutImageGeneratorModelSelector().call_CutImageGenerator_ai(
+            self.ai_model, 
+            prompt_text = prompt, 
+            prompt_images = image_paths
+        )
+        cut_image = image_generator_model.execute()
 
         filename = f"S{self.scene_num:04d}-C{cut_id:04d}.png"
         save_path = os.path.join(self.output_path, filename)
         os.makedirs(self.output_path, exist_ok=True)
-        self.cut_image.save(save_path)
+        cut_image.save(save_path)
 
         return save_path
-        # --------for test, will be deleted -------------------
 
-# -------------- For test ----------------------
-
-# if __name__ == "__main__":
-
-#     filtered_result = [
-#     ('시내버스_front.png', '시내버스 : 크기: 대형(길이 약 10m, 40인승), 색깔: 파란색(서울 시내버스 기준, 임의), 형체: 직사각형, 창문이 큼, 카테고리: 교통수단, 태그: 대중교통, 정류장, 종점, 기사, 승객')
-#     ]
-
-#     generator = CutImageGenerator(entity=filtered_result)
-#     generator.cut = {
-#     'cut_id': 1,
-#     'description': '버스 정류장에서 버스가 도착하는 장면.',
-#     'characters': [],
-#     'background': '조용한 아침 거리와 버스 정류장.',
-#     'objects': ['버스', '버스 정류장']
-#     }
-
-#     img = generator.execute()
-#     img.show()
-#     img.save("edited_output.png")
