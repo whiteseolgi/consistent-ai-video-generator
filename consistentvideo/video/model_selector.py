85be12da2f243e230bc4b397d82a4cf31e275851
rachihyeon <skallook@hanmail.net>
2025-08-07 04:19:28 +0900

    feat: VideoGeneratorModelRunway클래스 내부 구현
    - VideoGenerator클래스 내 동영상 생성 알고리즘 VideoGeneratorModelRunway클래스로 이전 및 동기화

--- consistentvideo/video/model_selector.py
+++ consistentvideo/video/model_selector.py
@@ -5,6 +5,9 @@ import base64
 import requests
 from PIL import Image
 from io import BytesIO
+from runwayml import RunwayML
+import time
+import re
 
 class CutImageGeneratorModelSelector:
     def __init__(self):
@@ -114,11 +117,88 @@ class VideoGeneratorModelSelector:
     def __init__(self):
         pass
 
-    def call_VideoGenerator_ai(self, ai_model : str, prompt_text : str = None, prompt_images : list = None):
+    def call_VideoGenerator_ai(self, ai_model : str, prompt_text : str = None, prompt_image : str = None):
         if ai_model == "runway":
-            return VideoGeneratorModelRunway(prompt_text = prompt_text, prompt_images = prompt_images)
+            return VideoGeneratorModelRunway(prompt_text = prompt_text, prompt_images = prompt_image)
         elif ai_model == "veo3":
-            return VideoGeneratorModelVeo3(prompt_text = prompt_text, prompt_images = prompt_images)
+            return VideoGeneratorModelVeo3(prompt_text = prompt_text, prompt_images = prompt_image)
         else:
             return None
 
+
+class VideoGeneratorModelRunway(VideoGeneratorAIBase):
+    def __init__(self, prompt_text : str = None, prompt_image : str = None):
+        api_key = os.getenv("RUNWAY_API_KEY")
+        super.ai_model = RunwayML(api_key=api_key)
+        super.prompt_text = prompt_text
+        super.prompt_image = prompt_image
+
+    def execute(self):
+
+        # 파일명에서 S번호와 C번호 추출
+        match = re.match(r'S(\d+)-C(\d+)', os.path.basename(super.prompt_image))
+        if match:
+            scene_num = int(match.group(1))  # S번호
+            cut_num = int(match.group(2))    # C번호
+
+        print(f"scene_num={scene_num}, cut_num={cut_num}")
+        cut = self.cut_list[scene_num-1][cut_num-1]
+        cut_id = cut.get('cut_id')
+        description = cut.get('description', '')
+        base_name = os.path.basename(image_path)
+        name_without_ext = os.path.splitext(base_name)[0]
+
+        prompt_text = f"{description} Make a video that fits this situation."
+
+        with open(super.prompt_image, "rb") as img_file:
+            encoded_image = base64.b64encode(img_file.read()).decode("utf-8")
+
+        task = self.ai_model.image_to_video.create(
+            model='gen4_turbo',
+            prompt_image=f"data:image/png;base64,{encoded_image}",
+            prompt_text=super.prompt_text,
+            ratio='1280:720',
+            duration=5,
+        )
+
+        task_id = task.id
+
+        time.sleep(10)
+        task = self.ai_model.tasks.retrieve(task_id)
+        while task.status not in ['SUCCEEDED', 'FAILED']:
+            time.sleep(10)
+            task = self.ai_model.tasks.retrieve(task_id)
+
+        # Download
+        if task.status == 'SUCCEEDED':
+            output_urls = task.output
+            if output_urls and isinstance(output_urls, list):
+                video_url = output_urls[0]
+                # filename = f"S{scene_num:04d}-C{cut_id:04d}_video.mp4"
+                # full_path = os.path.join(self.output_path, filename)
+
+                response = requests.get(video_url)
+                if response.status_code == 200:
+                    # with open(full_path, "wb") as f:
+                    #     f.write(response.content)
+                    # results.append(full_path)
+                    return response.content # Binary type video data
+                else:
+                    print(f"[cut_id={cut_id}] Error status code: {response.status_code}")
+            else:
+                print(f"[cut_id={cut_id}] no output url")
+        else:
+            print(f"[cut_id={cut_id}] fail to generate video: {task.status}")
+
+        return None
+
+
+class VideoGeneratorModelVeo3(VideoGeneratorAIBase):
+    '''
+    Need to fill this blank with Gemini version model
+    '''
+    def __init__(self, prompt_text : str = None, prompt_images : list = None):
+        pass
+
+    def execute(self):
+        pass
